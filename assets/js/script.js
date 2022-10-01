44e80101b4f3abda990afd57f2a4ed97fb3efe23
rickydamarsaputra <retrocode.rc@gmail.com>
2022-10-01 18:43:38 +0700

    finish

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=0d63e786c8954ad492ba645bee8f3a36';
+const content = document.querySelector('#content');
+
+fetch(NEWS_API_URL)
+	.then((res) => res.json())
+	.then((res) => {
+		let articleContent = '';
+		const { articles } = res;
+
+		articles.slice(0, 10).forEach((article) => {
+			articleContent += `
+      <article>
+        <img src="${article.urlToImage}" alt="${article.title}" />
+        <a href="${article.url}">${article.title}</a>
+        <p>${article.description}</p>
+      </article>
+      `;
+		});
+
+		content.innerHTML = articleContent;
+	});
